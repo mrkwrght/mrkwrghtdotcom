b712ff86c3fbf500108c1b998c5263af133b8910
Mark Wright <markwright785@gmail.com>
2017-08-17 21:32:37 +0100

    revert

--- assets/js/color.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-  $('.random' ).each(function(index) {
-    var color = "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-    var targetBox = document.getElementsByClassName("random")[index];
-    targetBox.style.color = "#" + color;
-
-  });
-});
